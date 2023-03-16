57978ad83cbf4703635d40eb6d3a70a3303cdae6
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-03-16 16:07:58 +0100

    feat: sezione contatti

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ createApp({
       contacts: [
         {
             name: 'Michele',
-            avatar: './img/avatar_1.png',
+            avatar: 'img/avatar_1.png',
             visible: true,
             messages: [
                 {
@@ -30,7 +30,7 @@ createApp({
         },
         {
             name: 'Fabio',
-            avatar: './img/avatar_2.png',
+            avatar: 'img/avatar_2.png',
             visible: true,
             messages: [
                 {
@@ -52,7 +52,7 @@ createApp({
         },
         {
             name: 'Samuele',
-            avatar: './img/avatar_3.png',
+            avatar: 'img/avatar_3.png',
             visible: true,
             messages: [
                 {
@@ -74,7 +74,7 @@ createApp({
         },
         {
             name: 'Giovanni',
-            avatar: './img/avatar_4.png',
+            avatar: 'img/avatar_4.png',
             visible: true,
             messages: [
                 {
@@ -91,7 +91,7 @@ createApp({
         },
         {
             name: 'Alessandro L.',
-            avatar: './img/avatar_5.png',
+            avatar: 'img/avatar_5.png',
             visible: true,
             messages: [
                 {
@@ -108,7 +108,7 @@ createApp({
         },
         {
             name: 'Claudia',
-            avatar: './img/avatar_5.png',
+            avatar: 'img/avatar_5.png',
             visible: true,
             messages: [
                 {
@@ -130,7 +130,7 @@ createApp({
         },
         {
             name: 'Federico',
-            avatar: './img/avatar_7.png',
+            avatar: 'img/avatar_7.png',
             visible: true,
             messages: [
                 {
@@ -147,7 +147,7 @@ createApp({
         },
         {
             name: 'Davide',
-            avatar: './img/avatar_8.png',
+            avatar: 'img/avatar_8.png',
             visible: true,
             messages: [
                 {
